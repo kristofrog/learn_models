4833d2241343caa7dc8d49b3c7f5adaee4ab217c
Krzysztof Rogozinski <csminespl@gmail.com>
2020-05-23 22:51:48 +0200

    Problem with rrtools.adin install

--- learn.R
+++ learn.R
@@ -1,1 +1,16 @@
-#GGPUBR
+#RRTOOLS
+
+pacman::p_load(tidyverse,ggpubr,devtools)
+
+devtools::install_github("benmarwick/rrtools")
+
+devtools::install_github("nevrome/rrtools.addin")
+
+#rlly dunno what is problem of that
+
+#Installing 2 packages: bsplus, shinyFiles
+#Installing packages into ‘C:/XXXX/XXXX/XXXXX/XXXXXX/R/win-library/4.0’
+#(as ‘lib’ is unspecified)
+#Error: Failed to install 'rrtools.addin' from GitHub:
+ # (converted from warning) unable to access index for repository http://www.stats.ox.ac.uk/pub/RWin/bin/windows/contrib/4.0:
+  #cannot open URL 'http://www.stats.ox.ac.uk/pub/RWin/bin/windows/contrib/4.0/PACKAGES'
